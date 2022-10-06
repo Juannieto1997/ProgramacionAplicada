21aabfdd4b6b1632dd29a4ad08aaa9cc4090a7b5
Juannieto1997 <47038637+Juannieto1997@users.noreply.github.com>
2022-10-06 16:21:43 -0500

    Add files via upload

---
+++ CORTE02/ParcialPractico02.py
@@ -0,0 +1,50 @@
+## PARCIAL 02 - Programación aplicada a sistemas mecatrónicos
+'''
+el siguiente parcial se debe realizar de manera individual solo pueden hacer uso de los archivos enviados como
+taller 02 así como las notas que tengan de clase.
+NOTA: puede modificar las funciones para que estas queden al interior de la clase según considere conveniente
+'''
+
+# TODO: implentar una nueva clase persona_bin, con un parametro adicional a persona (la edad de la persona)
+#   teniendo en cuenta que solo se deben tener dos nodos relacionados (1.0)
+
+def tree2bin (tree):
+    '''
+
+    :param tree:
+    :return:
+    '''
+    # TODO: implentar una función que permita converitir un arbol (como se utilizó en el taller 02) en un arbol
+    #   binario siguiendo el parametro de la edad. (1.0)
+
+def search(nombre,edad)
+    '''
+    Realice un algoritmo de busqueda que le permita buscar a una persona en su estructura teniendo en cuenta la edad y/o el nombre de la persona
+    esta busqueda se puede realizar con uno o ambos parametros, en caso de tener los dos parametros ambos deben coincidir
+    :param nombre: nombre de la persona que se desea buscar
+    :param edad: Edad de la persona que se desea buscar
+    :return: la persona deseada, en caso de no encontrar debe retornar -1
+    '''
+
+    #TODO: implementar la función descrita anteriormente. (1.0)
+
+def time (nombre,edad): 
+    ''' 
+    Esta fución busca comparar el tiempo de busqueda entre las dos estructuras, debe comparar el tiempo de ejecución entre: 
+	función de busqueda del taller 01 vs fucnión de busqueda implementada anteriormente (teniendo únicamente el nombre)
+	función de busqueda del taller 01 vs fucnión de busqueda implementada anteriormente (teniendo en cuenta el nombre y la edad)
+	función de busqueda implementada anteriormente (con edad) vs fucnión de busqueda implementada anteriormente (teniendo únicamente el nombre)
+    esta función no debe retornar nada, solo debe imprimir en pantalla el resultado de las comparaciones y cual de los algoritmos es más eficiente. 
+    :param nombre: nombre de la persona que se desea buscar
+    :param edad: Edad de la persona que se desea buscar
+    '''
+    # TODO: implementar la función descrita anteriormente (1.0)
+
+def balance (tree): 
+    '''
+    esta función debe tomar un arbol, balancearlo y comparar el tiempo de que toma la busqueda.
+    debe imprimir cuanto se demora cada uno de algoritmos de busqueda. 
+    :param tree: árbol sin balancear 
+    :return: el arbol balanceado 
+    '''
+    #TODO: implementar la función descrita anteriormente (1.0)
